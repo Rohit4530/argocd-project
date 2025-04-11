81b5ad2435f95df103cd7f516a0e9a6de0c792c1
rohit4530 <rohitgunjal547@gmail.com>
2025-04-11 21:00:46 +0530

    added 2ed application in project-1

--- argo-cd.tf
+++ argo-cd.tf
@@ -1,26 +1,3 @@
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.9.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.22.0"
-    }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
 # Argo CD installation
 resource "helm_release" "argocd" {
   name       = "argocd"
---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.22.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
