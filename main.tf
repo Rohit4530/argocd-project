efb06dad5b291407751c5b58602463c1dc2fade9
rohit4530 <rohitgunjal547@gmail.com>
2025-04-11 16:12:39 +0530

    changed replica from 3 to 2

--- main.tf
+++ main.tf
@@ -1,15 +1,33 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.22.0"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+# Argo CD installation
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = "argocd"
-  version    = "5.46.8" # You can check latest on Artifact Hub
+  version    = "5.46.8"
 
   create_namespace = true
 
@@ -17,6 +35,8 @@ resource "helm_release" "argocd" {
     file("values.yaml")
   ]
 }
+
+# NGINX Ingress installation
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   namespace  = "ingress-nginx"
@@ -29,8 +49,8 @@ resource "helm_release" "nginx_ingress" {
     yamlencode({
       controller = {
         extraArgs = {
-          "enable-ssl-passthrough" = ""
-          "enable-ssl-chain-completion" = "false"
+          "enable-ssl-passthrough"        = ""
+          "enable-ssl-chain-completion"   = "false"
         }
         service = {
           type = "NodePort"
@@ -42,5 +62,32 @@ resource "helm_release" "nginx_ingress" {
       }
     })
   ]
+}
+
+# Argo CD Application for NGINX
+resource "kubernetes_manifest" "nginx_app" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "nginx-app"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/Rohit4530/argocd-project.git"
+        targetRevision = "master"
+        path           = "argocd-applications/helm/nginx"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "default"
+      }
+      syncPolicy = {
+        automated = {}
+      }
+    }
+  }
 }
 
