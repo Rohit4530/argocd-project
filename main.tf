ad2fb029c16c047a519c9b80e98122b895fd7c36
rohit4530 <rohitgunjal547@gmail.com>
2025-04-11 14:17:58 +0530

    adde nginx application

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  version    = "5.46.8" # You can check latest on Artifact Hub
+
+  create_namespace = true
+
+  values = [
+    file("values.yaml")
+  ]
+}
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  namespace  = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      controller = {
+        extraArgs = {
+          "enable-ssl-passthrough" = ""
+          "enable-ssl-chain-completion" = "false"
+        }
+        service = {
+          type = "NodePort"
+          nodePorts = {
+            http  = 30080
+            https = 30443
+          }
+        }
+      }
+    })
+  ]
+}
+
